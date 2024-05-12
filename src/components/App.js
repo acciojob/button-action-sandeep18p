33b566b3383adb68e951194aa8fbf2d127456a4c
sandeep18p <sandeep.pansari98@gmail.com>
2024-05-13 00:43:38 +0530

    one

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,33 @@
-import React, { useState } from "react";
-import './../styles/App.css';
+import React, { useState, useRef, useEffect } from "react";
+import "./../styles/App.css";
 
-const App = (props) => {
+const App = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const paraRef = useRef(null);
+
+  const toggleVisibility = () => {
+    setIsVisible((prevVisible) => !prevVisible);
+  };
+
+  useEffect(() => {
+    // Accessing the inner HTML of the paragraph element
+    console.log(paraRef.current.innerHTML);
+    if (isVisible) {
+      paraRef.current.innerHTML =
+        "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy";
+    } else {
+      paraRef.current.innerHTML = "";
+    }
+  }, [isVisible]);
 
   return (
-    <div className="App" id="main">
-      // Do not alter the main div
+    <div id="main">
+      <p id="para" className={isVisible ? "show" : "hide"} ref={paraRef}></p>
+      <button id="click" onClick={toggleVisibility}>
+        Click Me
+      </button>
     </div>
   );
-}
+};
 
-export default App
+export default App;
